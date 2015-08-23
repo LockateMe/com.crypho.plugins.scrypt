6960984fa4bd4bc1de478d8a601ea839c54fbbdb
Ahmad Ben Mrad <batikhsouri@gmail.com>
2015-08-23 23:31:01 +0200

    More verification of parameters

--- www/scrypt.js
+++ www/scrypt.js
@@ -1,14 +1,30 @@
 var scrypt = function(successCallback, errorCallback, message, salt, options) {
     if (typeof errorCallback != "function")  {
-        console.log("ScryptPlugin.scrypt failure: failure parameter not a function");
+        console.error("ScryptPlugin.scrypt failure: failure parameter not a function");
         return;
     }
 
     if (typeof successCallback != "function") {
-        console.log("ScryptPlugin.scrypt failure: success callback parameter must be a function");
+        console.error("ScryptPlugin.scrypt failure: success callback parameter must be a function");
         return;
     }
     options = options || {};
+    if (options.N && !(typeof options.N == 'number' && Math.floor(options.N) == options.N && options.N > 0)){
+        console.error('ScryptPlugin.scrypt failure: when defined, options.N must be a strictly positive integer number');
+        return;
+    }
+    if (options.dkLen && !(typeof options.dkLen == 'number' && Math.floor(options.dkLen) == options.dkLen && options.dkLen > 0)){
+        console.error('ScryptPlugin.scrypt failure: when defined, options.dkLen must be a strictly positive integer number');
+        return;
+    }
+    if (options.r && !(typeof options.r == 'number' && Math.floor(options.r) == options.r && options.r > 0)){
+        console.error('ScryptPlugin.scrypt failure: when defined, options.r must be a strictly positive integer number');
+        return;
+    }
+    if (options.p && !(typeof options.p == 'number' && Math.floor(options.p) == options.p && options.p > 0)){
+        console.error('ScryptPlugin.scrypt failure: when defined, options.p must be a strictly positive integer number');
+        return;
+    }
     cordova.exec(successCallback, errorCallback, "ScryptPlugin", "scrypt", [message, salt, options]);
 };
 
